b661cc67b5c2b34086fce000106c50d9ba684c7d
unknown <ranaot56@gmail.com>
2025-02-06 23:06:43 +0600

    fix deploy issues

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://bycicle-store-client-site.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
